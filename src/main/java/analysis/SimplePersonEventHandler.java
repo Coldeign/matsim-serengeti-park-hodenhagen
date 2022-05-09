13aaf3cc018ee2f9ab67068d53dec2aad6288238
Coldeign <carlhoefer@gmx.de>
2022-05-09 17:47:48 +0200

    Reduced number of iterations from 100 to 10. Added simple event handler.

---
+++ src/main/java/analysis/SimplePersonEventHandler.java
@@ -0,0 +1,22 @@
+package analysis;
+
+import org.matsim.api.core.v01.events.PersonArrivalEvent;
+import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+
+public class SimplePersonEventHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler {
+    @Override
+    public void handleEvent(PersonArrivalEvent personArrivalEvent) {
+
+
+        System.out.println("Arrival: " + personArrivalEvent.getTime() + ": " + personArrivalEvent.getPersonId());
+    }
+
+    @Override
+    public void handleEvent(PersonDepartureEvent personDepartureEvent) {
+
+
+        System.out.println("Departure: " + personDepartureEvent.getTime() + ": " + personDepartureEvent.getPersonId());
+    }
+}
